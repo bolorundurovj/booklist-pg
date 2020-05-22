bb98101c994c18141e5169b45ca947de8b1a0f4d
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-22 19:52:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const mustacheExpress = require('mustache-express');
 
 const app = express();
 
+const mustache = mustacheExpress();
+mustache.cache = null;
+app.engine('mustache', mustache);
+app.set('view-engine', 'mustache')
+
 let PORT = 5005;
 
 app.use(express.static('public'));
+
+app.get('/list', (req, res) => {
+    res.send('Book Lists')
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);    
